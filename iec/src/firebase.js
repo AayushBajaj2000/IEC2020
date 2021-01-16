84d7314c13ac4b6c0e17f25d79df4795c968d787
Aayush Bajaj <59947649+AayushBajaj2000@users.noreply.github.com>
2021-01-15 23:03:56 -0500

    Update firebase.js

--- iec/src/firebase.js
+++ iec/src/firebase.js
@@ -2,15 +2,6 @@ import firebase from "firebase";
 
 // the firebaseApp for the initialization of the firebase
 const firebaseApp = firebase.initializeApp({
-
-    apiKey: "AIzaSyDdeFoYKvlcEWfdzifzo6ejdJmD8VCxANA",
-    authDomain: "contactless-payments-b6ea7.firebaseapp.com",
-    projectId: "contactless-payments-b6ea7",
-    storageBucket: "contactless-payments-b6ea7.appspot.com",
-    messagingSenderId: "1078152858804",
-    appId: "1:1078152858804:web:4d518bfbeb8f63aae71be1",
-    measurementId: "G-HCCWYXMV1B"
-
 });
 
 const db = firebaseApp.firestore();
